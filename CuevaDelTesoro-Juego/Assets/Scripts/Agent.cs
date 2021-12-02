bf719ce9020d81abe6a24077419f3e3bc7b4a996
kralcazar <kr.alcazar@gmail.com>
2021-12-02 21:17:42 +0100

    Nueva estructura de la base de conocimientos
    <Cell, <Epoch, Knowledge>>
    
    Falta preguntar si una acción es segura tanto si se tiene el tesoro como si no. Si tiene el tesoro buscará el camino de vuelta de lo contrario irá iterando entre las casillas seguras.

--- CuevaDelTesoro-Juego/Assets/Scripts/Agent.cs
+++ CuevaDelTesoro-Juego/Assets/Scripts/Agent.cs
@@ -4,32 +4,40 @@ using UnityEngine;
 
 public class KnowledgeBase
 {
-    //<Epoch, <Cell, Knowledge>>
-    private Dictionary<int, Dictionary<Vector2, bool[]>> knowledge = new Dictionary<int, Dictionary<Vector2, bool[]>>();
+    //<Cell, <Epoch, Knowledge>>
+    private Dictionary<Vector2, Dictionary<int, bool[]>> knowledge = new Dictionary<Vector2, Dictionary<int, bool[]>>();
 
-    public void Inform(int epoch, Vector2 gridPosition, bool[] knowledgeVector)
+    //<Cell, Visited>
+    private Dictionary<Vector2, bool> knowledgeVisited = new Dictionary<Vector2, bool>();
+
+    public void Inform(Vector2 gridPosition, bool[] knowledgeVector, int epoch)
     {
-        Dictionary<Vector2, bool[]> cellPerception;
-        if (!knowledge.ContainsKey(epoch))
+        Dictionary<int, bool[]> cellPerception;
+        if (!knowledge.ContainsKey(gridPosition))
         {
-            cellPerception = new Dictionary<Vector2, bool[]>();
-            cellPerception.Add(gridPosition, knowledgeVector);
-            knowledge.Add(epoch, cellPerception);
+            cellPerception = new Dictionary<int, bool[]>();
+            cellPerception.Add(epoch, knowledgeVector);
+            knowledge.Add(gridPosition, cellPerception);
         }
         else
         {
-            knowledge[epoch].Add(gridPosition, knowledgeVector);
+            knowledge[gridPosition].Add(epoch, knowledgeVector);
         }
     }
 
-    public void Ask()
+    public void InformAction(Vector2 gridPosition)
     {
-
+        knowledgeVisited.Add(gridPosition, true);
     }
 
-    public void PerformAction()
+    //Si la posición preguntada es segura
+    public bool Ask(Vector2 gridPosition)
     {
+        //Si no tiene el tesoro (iterar por celdas seguras)
 
+        //Si tiene el tesoro (volver por las celdas ok)
+
+        return false;
     }
 }
 
@@ -68,29 +76,41 @@ public class Enviroment
     }
 }
 
+public enum Directions { West, North, East, South }
+
+public class Action
+{
+    public Directions direction;
+}
+
 public class Agent : MonoBehaviour
 {
     private KnowledgeBase kb;
     private int tickCounter;
-    private bool found;
 
     // Start is called before the first frame update
     void Start()
     {
         kb = new KnowledgeBase();
-        PercieveAndInformSurroundings(transform.position);
+        PercieveAndInformEnvironmentSurroundings(transform.position, tickCounter);
     }
 
     // Update is called once per frame
     private void Tick()
     {
-        //poseer determinado conocimiento del entorno
-        //kb.PerformAction(perceptions, ); //Tomar acciones en base a las percepciones y conocimiento
-        PercieveAndInformSurroundings(transform.position);
+        PercieveAndInformEnvironmentSurroundings(transform.position, tickCounter);
+        Action action;
+        if ((action = AskForActions(transform.position)) != null)
+        {
+            int x, y;
+            GridManager.GetGrid().GetXY(transform.position, out x, out y);
+            kb.InformAction(new Vector2(x, y));
+            tickCounter++;
+        }
     }
 
     //Recibir percepciones del entorno e informar a la base de conocimientos
-    private void PercieveAndInformSurroundings(Vector3 worldPosition)
+    private void PercieveAndInformEnvironmentSurroundings(Vector3 worldPosition, int tickCounter)
     {
         int[] lookX = { -1, 0, 0, 1 };
         int[] lookY = { 0, -1, 1, 0 };
@@ -100,7 +120,43 @@ public class Agent : MonoBehaviour
         {
             int _x = x + lookX[i];
             int _y = y + lookY[i];
-            kb.Inform(tickCounter, new Vector2(_x, _y), Enviroment.GetPerception(new Vector2(_x, _y)));
+            kb.Inform(new Vector2(_x, _y), Enviroment.GetPerception(new Vector2(_x, _y)), tickCounter);
         }
+    }
+
+    private Action AskForActions(Vector3 worldPosition)
+    {
+        int x, y;
+        GridManager.GetGrid().GetXY(worldPosition, out x, out y);
+
+        int[] lookX = { -1, 0, 1, 0 };
+        int[] lookY = { 0, -1, 0, 1 };
+        for (int i = 0; i < lookX.Length; i++)
+        {
+            int _x = x + lookX[i];
+            int _y = y + lookY[i];
+            if(kb.Ask(new Vector2(_x, _y)))
+            {
+                Action action = new Action();
+                switch (i)
+                {
+                    case 0:
+                        action.direction = Directions.West;
+                        break;
+                    case 1:
+                        action.direction = Directions.North;
+                        break;
+                    case 2:
+                        action.direction = Directions.East;
+                        break;
+                    case 3:
+                        action.direction = Directions.South;
+                        break;
+                }
+                transform.position = GridManager.GetGrid().GetWorldPosition(_x, _y);
+                return action;
+            }
+        }
+        return null;
     }
 }
